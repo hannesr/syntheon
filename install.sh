a186b3488723128be698fef84c6b3a5f8ea130b4
Hannes R <hannes.romppainen@iki.fi>
2019-04-05 20:50:02 +0300

    progress info for install

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 SRC_DIR=.
 TARGET_DIR=/opt/syntheon
 
-# check preconditions
+echo "... checking preconditions"
 if [ $(id -u) -ne 0 ]; then
   echo "Script must be run as root"
   exit 1
@@ -28,7 +28,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# copy files
+echo "... copying files"
 mkdir -p $TARGET_DIR
 cp -r $SRC_DIR/systemd $TARGET_DIR
 cp -r $SRC_DIR/control $TARGET_DIR
@@ -37,15 +37,19 @@ mkdir -p $TARGET_DIR/data
 # jack installation
 systemctl is-active jack &>/dev/null
 if [ $? -ne 0 ]; then
+  echo "... installing jack service"
   ln -sf $TARGET_DIR/systemd/jack.service /etc/systemd/system/
   systemctl daemon-reload
   systemctl enable jack
   systemctl start jack
+else
+  echo "... jack service already installed"
 fi
 
 # rakarrack installation
 systemctl is-active rakarrack &>/dev/null
 if [ $? -ne 0 ]; then
+  echo "... installing rakarrack service"
   # TODO: check if rakarrack configuration is in place
   # if not, create configuration from scratch
   # if it is, edit some keys
@@ -53,21 +57,31 @@ if [ $? -ne 0 ]; then
   systemctl daemon-reload
   systemctl enable rakarrack
   systemctl start rakarrack
+else
+  echo "... rakarrack service already installed"
 fi
 
 # zynaddsubfx installation
 systemctl is-active zynaddsubfx &>/dev/null
 if [ $? -ne 0 ]; then
+  echo "... installing zynaddsubfx service"
   ln -sf $TARGET_DIR/systemd/zynaddsubfx.service /etc/systemd/system/
   systemctl daemon-reload
   # note: service is started manually
+else
+  echo "... zynaddsubfx service already installed"
 fi
 
 # syntheon control process installation
 systemctl is-active syntheon &>/dev/null
 if [ $? -ne 0 ]; then
+  echo "... installing syntheon service"
   ln -sf $TARGET_DIR/systemd/syntheon.service /etc/systemd/system/
   systemctl daemon-reload
   systemctl enable syntheon
   systemctl start syntheon
+else
+  echo "... syntheon service already installed"
 fi
+
+echo "... install complete"
